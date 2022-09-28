edf32fcf5674eea17d8574333a76efa6ff3575fa
BlazeIsClone <64116737+BlazeIsClone@users.noreply.github.com>
2022-09-28 23:50:42 +0530

    feat: add dark/light mode compilers

--- src/build.js
+++ src/build.js
@@ -6,9 +6,27 @@ const typingsES6Template = _.template(
 	fs.readFileSync(`${__dirname}/templates/es6.template`)
 );
 
+const scssTemplate = _.template(
+	fs.readFileSync(`${__dirname}/templates/scss.template`)
+);
+
+const cssTemplate = _.template(
+	fs.readFileSync(`${__dirname}/templates/css.template`)
+);
+
 StyleDictionary.registerFormat({
 	name: 'typings/es6',
 	formatter: typingsES6Template,
+});
+
+StyleDictionary.registerFormat({
+	name: 'schema/scss',
+	formatter: scssTemplate,
+});
+
+StyleDictionary.registerFormat({
+	name: 'schema/css',
+	formatter: cssTemplate,
 });
 
 StyleDictionary.buildAllPlatforms();
