4f859965d96023942c5d7c27c83c87af0368787f
BlazeIsClone <64116737+BlazeIsClone@users.noreply.github.com>
2022-09-29 19:39:57 +0530

    add: spacing tokens && type tokens beta

--- src/build.js
+++ src/build.js
@@ -3,20 +3,18 @@ const _ = require('lodash');
 const StyleDictionary = require('style-dictionary').extend('./config.json');
 
 const typingsES6Template = _.template(
-	fs.readFileSync(`${__dirname}/templates/types.template`)
+	fs.readFileSync(`${__dirname}/templates/types.ejs`)
 );
 
 const scssTemplate = _.template(
-	fs.readFileSync(`${__dirname}/templates/scss.template`)
+	fs.readFileSync(`${__dirname}/templates/scss.ejs`)
 );
 
 const cssTemplate = _.template(
-	fs.readFileSync(`${__dirname}/templates/css.template`)
+	fs.readFileSync(`${__dirname}/templates/css.ejs`)
 );
 
-const jsTemplate = _.template(
-	fs.readFileSync(`${__dirname}/templates/js.template`)
-);
+const jsTemplate = _.template(fs.readFileSync(`${__dirname}/templates/js.ejs`));
 
 StyleDictionary.registerFormat({
 	name: 'typings/types',
